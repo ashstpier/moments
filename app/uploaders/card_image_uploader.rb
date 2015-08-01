a9e73e95103ee780e4e74928dea211441fc7db67
Ashley St Pier <ashleystpier@gmail.com>
2015-08-01 11:37:57 +0100

    update seeds

--- app/uploaders/card_image_uploader.rb
+++ app/uploaders/card_image_uploader.rb
@@ -29,7 +29,7 @@ class CardImageUploader < CarrierWave::Uploader::Base
   # end
 
   # Process files as they are uploaded:
-  process resize_to_fill: [1024, 720]
+  process resize_to_fill: [1200, 800]
 
   # Create different versions of your uploaded files:
   # version :thumb do
